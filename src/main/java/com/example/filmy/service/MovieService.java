fb1a9af49b1e06fa3dd39ba66e0c56a54806b0f6
szatanmilio <jediszatanmilio@gmail.com>
2022-06-07 13:43:09 +0200

    Commit 1.18: Oczyszczanie TO DO opcjonalne: rating userów

--- src/main/java/com/example/filmy/service/MovieService.java
+++
@@ -1,9 +0,0 @@
-//package com.example.filmy.service;
-//
-//import com.example.filmy.model.Movie;
-//import com.example.filmy.web.dto.MovieSaveDto;
-//import org.springframework.security.core.userdetails.UserDetailsService;
-//
-//public interface MovieService {
-//	Movie save(MovieSaveDto movieToSave);
-//}
